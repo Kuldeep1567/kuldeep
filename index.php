a5c00f20dd84cf7ef429a8d0556219911abb28dd
Kuldeep1567 <165204346+Kuldeep1567@users.noreply.github.com>
2024-03-27 14:58:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <dancename>Dashboard</dancename>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
